34c501333d58bb36e5c42928445784162cd7f044
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-26 21:17:19 +0600

    first commit

---
+++ __server_deploy.js
@@ -0,0 +1,14 @@
+/**
+ * ---------------------
+ * ONE TIME for your computer
+ * 1. create account
+ * 2.verify account
+ * 3.install heroku cli
+ * 4.heroku login
+ * ----------------------
+ * For each project one time
+ * 1. heroku create
+ * 2. make sure you: git add . git commit. git push
+ * 
+ * 
+ */
