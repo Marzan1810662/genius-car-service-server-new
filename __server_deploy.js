3dd8755568329e27ff733429c70a7199e05ad0b5
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-26 21:41:11 +0600

    adeed hero endpoint

--- __server_deploy.js
+++ __server_deploy.js
@@ -9,6 +9,18 @@
  * For each project one time
  * 1. heroku create
  * 2. make sure you: git add . git commit. git push
+ * 3. git push heroku main
+ * 4. go to heroku dashboard > current project > settings > reavel config vars
+ * 5.caopy paste config vars from your .env file
+ * 6. make sure you have whitelisted all ip address to access mongodb
+ * -------------------------
+ * UPDATE SERVER
+ * ------------------------
+ * 1. make changes
+ * 2. make sure you: git add . git commit. git push
+ * 3. git push heroku main
+ * -------------------------
+ * Connect server with client
  * 
  * 
  */
